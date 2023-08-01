f900c853b13ffcc56f8619228716c4f54385c586
andr33aurs <dr.ursachi@gmail.com>
2023-08-01 11:25:19 +0300

    added color

--- code_file.py
+++ code_file.py
@@ -13,40 +13,41 @@ def generate_prompt_table(field_names, groups):
 
 
 def show_breeds_in_groups():
-    # print(Fore.MAGENTA + "Welcome to the Dog Breed Choice!")
-    # print(Style.RESET_ALL)
-    # time.sleep(2)
-    # print(logo)
-    # time.sleep(2)
-    # print(Fore.MAGENTA + "You came here because you want to choose the best dog breed for your family.")
-    # time.sleep(2)
-    # print(Fore.CYAN + "But first, you need to know that dog breeds are divided into 10 classes as follows:")
-    #
-    # time.sleep(2)
+    print(Fore.MAGENTA + "Welcome to the Dog Breed Choice!")
+    print(Style.RESET_ALL)
+    time.sleep(2)
+    print(logo)
+    time.sleep(2)
+    print(Fore.MAGENTA + "You came here because you want to choose the best dog breed for your family.")
+    time.sleep(2)
+    print("But first, you need to know that dog breeds are divided into 10 classes as follows:")
 
-    # time.sleep(5)
-
+    time.sleep(2)
+    print(Fore.CYAN)
     generate_prompt_table(["Group Number", "Group Name"], breed_group)
+    print(Style.RESET_ALL)
     chosen_group_number = int(input("To learn more about each group, "
                                     "please choose one of the above by typing the number of the group: \n"))
-    print(Style.RESET_ALL)
+
     group = breed_group[chosen_group_number]
-    print(group["name"])
+    # print(group["name"])
     subgroups = group["subgroups"]
-    for breed_key, breed_value in subgroups.items():
-        print(f"{breed_key}: {breed_value['name']}")
 
+    # for breed_key, breed_value in subgroups.items():
+    #     print(f"{breed_key}: {breed_value['name']}")
+    print(Fore.MAGENTA)
     generate_prompt_table(["Breed Number", "Breed Name"], subgroups)
-
+    print(Style.RESET_ALL)
     breed_nr = int(input("Please add the number corresponding to the wanted breed:"))
     breed = subgroups[breed_nr]
     breed_characteristics = breed["characteristics"]
 
-    for characteristic_key, characteristic_value in breed_characteristics.items():
-        print(f"{characteristic_key}: {characteristic_value['name']}")
-
+    # for characteristic_key, characteristic_value in breed_characteristics.items():
+    #     print(f"{characteristic_key}: {characteristic_value['name']}")
+    print(Fore.CYAN)
     generate_prompt_table(["Characteristic Number", "Characteristic"], breed_characteristics)
-
+    print(Style.RESET_ALL)
     characteristic_nr = int(input("Please add the number corresponding to the wanted breed:"))
+    print(Fore.LIGHTGREEN_EX)
     characteristic = breed_characteristics[characteristic_nr]
     print(characteristic["value"])
